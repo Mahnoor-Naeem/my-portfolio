84b4efb15a4d8293fe66310e7555adc25e5db4a9
Mahnoor-Naeem <mahnoornaeemqureshi@gmail.com>
2020-12-21 21:42:46 +0500

    added files

---
+++ index.js
@@ -0,0 +1,19 @@
+
+AOS.init({
+  duration: 1500,
+});
+
+// Get the container element
+var btnContainer = document.getElementById("navbar");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("nav-item");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
